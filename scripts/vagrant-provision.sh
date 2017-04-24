2b97b0a1529702925ce376a9a26006dad6004724
Fabio Kreusch <fabiokr@gmail.com>
2017-04-24 11:23:26 -0300

    Copies git bins

--- scripts/vagrant-provision.sh
+++ scripts/vagrant-provision.sh
@@ -6,6 +6,8 @@ cp /tmp/provision/zshrc ~/.zshrc
 cp /tmp/provision/bundle ~/.bundle/config
 cp /tmp/provision/gemrc ~/.gemrc
 cp /tmp/provision/gitconfig ~/.gitconfig
+cp /tmp/provision/git-pretty-log ~/bin/git-pretty-log
+cp /tmp/provision/git-sweep ~/bin/git-sweep
 
 # install bundler
 gem install --user-install bundler pry
